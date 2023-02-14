355fad898330404f7e25b601f706823f1d4ef5e3
Michelle_Mungai <chellymungai@gmail.com>
2023-02-14 14:37:14 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :height, :base, :hypotenuse
+
+  def initialize (height, base, hypotenuse)
+    @height = height
+    @base = base
+    @hypotenuse = hypotenuse
+  end
+  def kind 
+    if @height > 0 && base > 0 && hypotenuse > 0 && @height + @base > @hypotenuse && @base + @hypotenuse > @height && @height + @hypotenuse > @base
+      sides = [@height, @base, @hypotenuse]
+      check = []
+      sides.each do |side|
+        check << sides.count(side)
+      end 
+      type = check.max
+      case type 
+      when 3 then :equilateral
+      when 2 then :isosceles
+      when 1 then :scalene
+    end
+  else 
+    raise(TriangleError)
+  end
+end
+class TriangleError < StandardError
+
+  def message
+    puts 'Not a triangle'
+  end
+end
 end
